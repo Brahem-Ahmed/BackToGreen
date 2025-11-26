5ff73d55883bc40be44e27049f95447d601a7a67
samarmanai33 <samarmanai823@gmail.comm>
2025-11-26 23:40:34 +0100

    Merge branch 'main' of https://github.com/Brahem-Ahmed/BackToGreen

---
+++ src/Form/EvenementEcologiqueType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\EvenementEcologique;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementEcologiqueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('lieu')
+            ->add('capaciteMax')
+            ->add('categorie')
+            ->add('idOrganisateur', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => EvenementEcologique::class,
+        ]);
+    }
+}
